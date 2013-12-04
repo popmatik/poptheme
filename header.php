6fd8f2f51cbd63501a7ded36c3a57a625043f8f1
Rob leach <popmatik@gmail.com>
2013-12-04 16:15:33 +0000

    Added HTML5 shiv and respond.js for IE8

--- header.php
+++ header.php
@@ -18,8 +18,12 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 <?php wp_head(); ?>
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
+      <script src="<?php echo get_template_directory_uri(); ?>/js/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
